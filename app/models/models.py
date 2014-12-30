c7167e6ccb18ebd51f190b5f5af90b72b400a791
q_brian <brian@qurius-inc.com>
2014-12-29 21:38:07 -0800

    playing with functions in models.

--- app/models/models.py
+++ app/models/models.py
@@ -1,2 +1,38 @@
+# -*- coding: utf-8 -*-
+from flask import current_app
+from sqlalchemy import func
 
-class Player(object):
+from . import db
+
+class PlayerCharacter(object):
+    def ability_bonus(self, ascore):
+        abonus = {(1):-5,
+            (2):-4,
+            (3):-4,
+            (4):-3,
+            (5):-3,
+            (6):-2,
+            (7):-2,
+            (8):-1,
+            (9):-1,
+            (10):0,
+            (11):0,
+            (12):1,
+            (13):1,
+            (14):2,
+            (15):2,
+            (16):3,
+            (17):3,
+            (18):4,
+            (19):4,
+            (20):5,
+            (21):5,
+            (22):5,
+            (23):6,
+            (24):6,
+            }
+
+        return abonus.get(ascore)
+
+
+
